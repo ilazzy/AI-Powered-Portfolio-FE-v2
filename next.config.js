7e7252aa89be218b0c42f9f82a99c347ae339408
Syed Ahamed <zyedrazer.22@gmail.com>
2025-11-08 19:54:42 +0530

    fix: change dp and resume and added redirect url for certificates

--- next.config.js
+++ next.config.js
@@ -12,4 +12,25 @@ const nextConfig = {
   devIndicators: false,
 };
 
-module.exports = withPWA(nextConfig);
+module.exports = withPWA({
+  ...nextConfig,
+  async redirects() {
+    return [
+      {
+        source: '/official-website/deeplearning',
+        destination: 'https://learn.deeplearning.ai/accomplishments/b97023e9-0bae-4cdf-8924-6886f112626f?usp=sharing',
+        permanent: true,
+      },
+      {
+        source: '/official-website/graphacademy',
+        destination: 'https://graphacademy.neo4j.com/c/eae960cb-4b90-4580-a379-6509ded1a8f7/',
+        permanent: true,
+      },
+      {
+        source: '/official-website/guvi',
+        destination: 'https://www.guvi.in/verify-certificate?id=SY17r4C46JR9100975',
+        permanent: true,
+      },
+    ];
+  },
+});
